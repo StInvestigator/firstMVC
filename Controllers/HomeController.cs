e18692bbba293bec179519ce0044a482665bb100
Murashko Vladislav <vladislavuh2000@gmail.com>
2024-05-13 16:22:54 +0300

    ht done

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,12 +17,6 @@ namespace firstMVC.Controllers
         {
             return View();
         }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         public IActionResult AboutMe()
         {
             AboutMe aboutMe = new AboutMe("Владислав", "Мурашко", 17, "народився в Краматорську, зараз перебуваю в Одесі", "/img/Gigachad.jpg");
@@ -37,6 +31,58 @@ namespace firstMVC.Controllers
             ViewData["Skills"] = skills;
             return View();
         }
+        
+        [HttpGet]
+        public IActionResult UserForm()
+        {
+            
+            User user = new User();
+            if (System.IO.File.Exists("D:\\SHAG\\visual save\\firstMVC\\wwwroot\\data\\UserData.txt"))
+            {
+                using (StreamReader reader = new StreamReader("D:\\SHAG\\visual save\\firstMVC\\wwwroot\\data\\UserData.txt"))
+                {
+                    string line = reader.ReadLine();
+                    if (line != null)
+                    {
+                        user.Name = line.Split(" ")[0];
+                        user.Password = line.Split(" ")[1];
+                    }
+                    reader.Close();
+                }
+            }
+            return View(user);
+        }
+
+        [HttpPost]
+        public IActionResult UserForm([FromForm] User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(user);
+            }
+            StreamWriter writer = new StreamWriter("D:\\SHAG\\visual save\\firstMVC\\wwwroot\\data\\UserData.txt");
+            writer.Write(user.Name + " " + user.Password);
+            writer.Close();
+            return Redirect("/Home/UserPage");
+        }
+
+        public IActionResult UserPage()
+        {
+            if (System.IO.File.Exists("D:\\SHAG\\visual save\\firstMVC\\wwwroot\\data\\UserData.txt"))
+            {
+                using (StreamReader reader = new StreamReader("D:\\SHAG\\visual save\\firstMVC\\wwwroot\\data\\UserData.txt"))
+                {
+                    string line = reader.ReadLine();
+                    if (line != null)
+                    {
+                        ViewData["UserName"] = line.Split(" ")[0];
+                    }
+                    reader.Close();
+                }
+            }
+            return View();
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace firstMVC.Models
+{
+    public class User
+    {
+        [DisplayName("Ім'я")]
+        [Required(ErrorMessage = "Ім'я обов'язкове")]
+        [MinLength(3, ErrorMessage = "Мінімум 3 символи")]
+        [MaxLength(20, ErrorMessage = "Максимум 20 символів")]
+        public string Name { get; set; }
+
+
+        [DisplayName("Пароль")]
+        [Required(ErrorMessage = "Пароль обов'язковий")]
+        [MinLength(8, ErrorMessage = "Мінімум 8 символів")]
+        [MaxLength(20, ErrorMessage = "Максимум 20 символів")]
+        public string Password { get; set; }
+    }
+}
